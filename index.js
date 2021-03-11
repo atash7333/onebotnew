b5081b92e5942b028bb40f3941419c93d9f4b79b
atash7333 <65875645+atash7333@users.noreply.github.com>
2021-03-11 03:49:05 +0300

    changing2222

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const request = require('request');
 
 let app = express();
 
-app.set("port", (process.env.port ||  3000));
+app.set("port", (process.env.PORT ||  8000));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
