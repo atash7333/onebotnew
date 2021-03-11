a46769f28569f94f24ebfa831eb9b4a3dfb69222
atash7333 <65875645+atash7333@users.noreply.github.com>
2021-03-11 04:38:33 +0300

    add webhook

--- index.js
+++ index.js
@@ -5,17 +5,30 @@ const request = require('request');
 
 let app = express();
 
-app.set("port", (process.env.PORT ||  8000));
+app.set("port", (process.env.PORT || 8000));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Display the web page
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send("hiiiiiiiiiiiiiiiiii");
 });
 
-app.listen(app.get("port"),function () {
-     console.log("ashraf");
- });
+
+app.get('/', function (req, res) {
+  const PAGE_VERIFY_TOKEN = "asdfghjkl;'";
+
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+  if (token == PAGE_VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  }
+  else {
+    res.sendStatus(403);
+  }
+});
+app.listen(app.get("port"), function () {
+  console.log("ashraf");
+});
